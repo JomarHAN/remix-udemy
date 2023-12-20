848d749facb9a98ba0aa70a088e7e8cee7404418
Jomar <jomarhan.ng@gmail.com>
2023-12-20 13:33:51 -0500

    start remix

---
+++ app/routes/demo.jsx
@@ -0,0 +1,11 @@
+import { Link } from "@remix-run/react";
+
+export default function Demo() {
+    return (
+        <>
+        <h1>Demo</h1>
+        <Link to='/'>Home</Link>
+        </>
+    )   
+}
+
