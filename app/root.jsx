1b7ca3ec769e05cc4195da0c2dcf94da4c24210f
Jomar <jomarhan.ng@gmail.com>
2023-12-21 16:42:49 -0500

    add and setup ErrorBoundary for root

--- app/root.jsx
+++ app/root.jsx
@@ -1,10 +1,13 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 
 import mainStyles from "~/styles/main.css";
@@ -33,3 +36,64 @@ export default function App() {
     </html>
   );
 }
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+          <title>Oh no! Wrong Route!</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main className="error">
+            <h1>Opps!</h1>
+            <p>Status: {error.status}</p>
+            <p>{error.data.message}</p>
+            <p>
+              Back to <Link to="/">safety</Link>!
+            </p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>Oh no! Error Happen!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+};
